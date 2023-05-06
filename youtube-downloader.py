904515fb086a131da63c8acc32d7e27549c58372
paichiwo <lzerucha@gmail.com>
2023-05-06 21:38:15 +0100

    Added functionality - = display thumbnails.

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -5,15 +5,30 @@
 import time
 import PySimpleGUI as sg
 import requests
+import io
+import os
 from pytube import YouTube
+from PIL import Image
 
 # https://youtu.be/eEQh4qvj-qo
+
+
+def jpg_to_png(url, title):
+    response = requests.get(url)
+    image = Image.open(io.BytesIO(response.content))
+    max_size = (200, 112)
+    image.thumbnail(max_size)
+    if not os.path.exists("thumbs"):
+        os.mkdir("thumbs")
+    file_path = os.path.join("thumbs/", "thumb.png")
+    image.save(file_path, "PNG")
+    return file_path
 
 
 def progress_check(stream, chunk, bytes_remaining):
     progress_amount = 100 - round(bytes_remaining / stream.filesize * 100)
     window['-PROGRESS-BAR-'].update(progress_amount,
-                                    bar_color=('blue', 'skyblue'))
+                                    bar_color=('blue', 'lightblue'))
     sg.popup("Done")
 
 
@@ -21,7 +36,8 @@ sg.theme('black')
 
 layout = [
     [sg.Stretch(), sg.Image("image/logo.png"), sg.Stretch()],
-    [sg.Text('Enter YouTube Video URL:', size=20), sg.InputText(key='-URL-')],
+    [sg.Stretch(), sg.Image("image/dummy_thumb.png", key="-THUMBNAIL-"), sg.Stretch()],
+    [sg.Text('Enter YouTube Video URL:', size=20), sg.InputText(enable_events=True, key='-URL-')],
     [sg.Button('Download', size=11, key="-DOWNLOAD-"),
      sg.Push(),
      sg.ProgressBar(max_value=100,
@@ -31,7 +47,7 @@ layout = [
                     bar_color=('#199FD0', '#FFFFFF'),
                     key='-PROGRESS-BAR-')],
     [sg.Table(values=[], headings=["Title", "Resolution", "ITag"],
-              col_widths=[50, 6, 4],
+              col_widths=[36, 6, 4],
               auto_size_columns=False,
               justification="center",
               selected_row_colors="red on white",
@@ -54,6 +70,7 @@ while True:
         break
 
     if event == "-DOWNLOAD-":
+        # update the table
         table = window["-TABLE-"]
         url = values["-URL-"]
         list_of_streams.clear()
@@ -61,6 +78,9 @@ while True:
         for stream in yt.streams.filter(file_extension="mp4").order_by("resolution").desc():
             list_of_streams.append([yt.title, stream.resolution, stream.itag])
             print(yt.title, stream.itag, stream.resolution, stream.itag)  # for DEBUGGING
+        # get thumbnail
+        thumbnail_string = jpg_to_png(yt.thumbnail_url, yt.title)
+        window["-THUMBNAIL-"].update(thumbnail_string)
         table.update(list_of_streams)
 
     if event == "-TABLE-":
