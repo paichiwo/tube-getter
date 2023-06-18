a56ce16c4f8f9da1fb1809c6c78e3069584b688e
paichiwo <lzerucha@gmail.com>
2023-06-18 13:40:01 +0100

    Reformat layout to a function

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -14,10 +14,12 @@ import os
 from pytube import YouTube
 from PIL import Image
 
+font_used = ("Tahoma", 9)
 
-def jpg_to_png(link):
+
+def jpg_to_png(url):
     """ Convert .jpg to .png to properly display thumbnail in PySimpleGUI """
-    response = requests.get(link)
+    response = requests.get(url)
     image = Image.open(io.BytesIO(response.content))
     image.thumbnail((300, 168))
     file_path = os.path.join("Downloads", "thumb.png")
@@ -34,8 +36,8 @@ def change_extension():
 
 
 def count_size():
-    """ Count the streams file sizes """
-    return round(stream.filesize / (1024 * 1024), 1)
+    """ Count the stream file sizes """
+    return round(stream_data.filesize / (1024 * 1024), 1)
 
 
 def download():
@@ -43,8 +45,7 @@ def download():
     choice = sg.popup_yes_no("Download?", font=font_used)
     if choice == "Yes":
         url = values["-URL-"]
-        yt = YouTube(url, use_oauth=True, allow_oauth_cache=True,
-                     on_progress_callback=progress_check, on_complete_callback=on_complete)
+        yt = YouTube(url, on_progress_callback=progress_check, on_complete_callback=on_complete)
         if not os.path.exists("Downloads"):
             os.mkdir("Downloads")
         stream = yt.streams.get_by_itag(i_tag)
@@ -68,31 +69,32 @@ def on_complete(stream, file_path):
     sg.popup("Done!")
 
 
-# Application layout
-font_used = ("Tahoma", 9)
+def create_window():
+    """Application layout."""
+    sg.theme("black")
 
-sg.theme("black")
+    layout = [
+        [sg.Stretch(), sg.Image("image/logo.png"), sg.Stretch()],
+        [sg.VPush()],
+        [sg.Stretch(), sg.Image("image/dummy_thumb.png", key="-THUMBNAIL-"), sg.Stretch()],
+        [sg.VPush()],
+        [sg.Text("Video URL:", font=font_used)],
+        [sg.Input(size=60, enable_events=True, key="-URL-")],
+        [sg.Button("Video", font=font_used, size=10, key="-VIDEO-"),
+         sg.Button("Audio", font=font_used, size=10, key="-AUDIO-")],
+        [sg.Table(values=[], headings=["Title", "Resolution", "Size", "Tag"],
+                  col_widths=[33, 7, 8, 4], auto_size_columns=False, justification="center", enable_events=True,
+                  font=font_used, selected_row_colors="red on white", expand_x=True, expand_y=True, key="-TABLE-")],
+        [sg.ProgressBar(max_value=100, orientation='h', border_width=0, size=(25, 2),
+                        bar_color=("red", "white"), expand_x=True, key='-PROGRESS-BAR-')],
+    ]
 
-layout = [
-    [sg.Stretch(), sg.Image("image/logo.png"), sg.Stretch()],
-    [sg.VPush()],
-    [sg.Stretch(), sg.Image("image/dummy_thumb.png", key="-THUMBNAIL-"), sg.Stretch()],
-    [sg.VPush()],
-    [sg.Text("Video URL:", font=font_used)],
-    [sg.Input(size=60, enable_events=True, key="-URL-")],
-    [sg.Button("Video", font=font_used, size=10, key="-VIDEO-"),
-     sg.Button("Audio", font=font_used, size=10, key="-AUDIO-")],
-    [sg.Table(values=[], headings=["Title", "Resolution", "Size", "Tag"],
-              col_widths=[33, 7, 8, 4], auto_size_columns=False, justification="center", enable_events=True,
-              font=font_used, selected_row_colors="red on white", expand_x=True, expand_y=True, key="-TABLE-")],
-    [sg.ProgressBar(max_value=100, orientation='h', border_width=1, size=(25, 7),
-                    bar_color=("red", "white"), expand_x=True, key='-PROGRESS-BAR-')],
-]
+    return sg.Window("YouTube Downloader by paichiwo", layout,
+                     element_justification="center", size=(520, 520), resizable=True)
 
-window = sg.Window("YouTube Downloader by paichiwo", layout,
-                   element_justification="center", size=(520, 520), resizable=True)
 
 list_of_streams = []
+window = create_window()
 
 while True:
     event, values = window.read()
@@ -100,34 +102,33 @@ while True:
         break
 
     if event == "-VIDEO-":
-        # Update the table with Video streams
-        url = values["-URL-"]
-        if url:
+        link = values["-URL-"]
+        if link:
             list_of_streams.clear()
-            yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
-            for stream in yt.streams.filter(file_extension="mp4").order_by("resolution").desc():
-                list_of_streams.append([yt.title, stream.resolution, f"{count_size()} MB", stream.itag])
+            yt_data = YouTube(link, use_oauth=False)  # use_oauth=True, allow_oauth_cache=True
+            for stream_data in yt_data.streams.filter(file_extension="mp4").order_by("resolution").desc():
+                list_of_streams.append([yt_data.title, stream_data.resolution, f"{count_size()} MB", stream_data.itag])
             window["-TABLE-"].update(list_of_streams)
-            # Get thumbnail
-            thumbnail_string = jpg_to_png(yt.thumbnail_url)
+
+            thumbnail_string = jpg_to_png(yt_data.thumbnail_url)
             window["-THUMBNAIL-"].update(thumbnail_string)
         else:
             sg.Popup("ERROR: No url detected.", font=font_used)
 
     if event == "-AUDIO-":
-        # Update the table with Audio streams
-        url = values["-URL-"]
-        if url:
+        link = values["-URL-"]
+        if link:
             list_of_streams.clear()
-            yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
-            for stream in yt.streams.filter(only_audio=True).order_by("abr").desc():
-                list_of_streams.append([yt.title, stream.abr, f"{count_size()} MB", stream.itag])
+            yt_data = YouTube(link)  # use_oauth=True, allow_oauth_cache=True
+            for stream_data in yt_data.streams.filter(only_audio=True).order_by("abr").desc():
+                list_of_streams.append([yt_data.title, stream_data.abr, f"{count_size()} MB", stream_data.itag])
             window["-TABLE-"].update(list_of_streams)
-            # Get thumbnail
-            thumbnail_string = jpg_to_png(yt.thumbnail_url)
+
+            thumbnail_string = jpg_to_png(yt_data.thumbnail_url)
             window["-THUMBNAIL-"].update(thumbnail_string)
         else:
             sg.Popup("ERROR: No url detected.", font=font_used)
+
 
     if event == "-TABLE-":
         try:
