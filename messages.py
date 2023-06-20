bc9e111a7071d764a50112b84ebac9ebe1ceb750
paichiwo <lzerucha@gmail.com>
2023-06-20 18:36:39 +0100

    Create messages.py with all the longer messages.

---
+++ messages.py
@@ -0,0 +1,7 @@
+popup_message = """
+The Python-based YouTube Getter enables users to download video or
+audio files from YouTube with ease. The user interface is built using
+the PySimpleGUI library, while the PyTube library is employed to
+implement the video downloading functionality. With a and user-friendly
+design, it offers a hassle-free method to obtain their desired YouTube content.
+"""
