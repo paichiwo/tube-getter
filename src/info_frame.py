66a4dfecc66e694f9b6d7306982bcf21e54a021f
paichiwo <lzerucha@gmail.com>
2024-04-29 17:24:57 +0100

    Implement full table prototype

--- src/info_frame.py
+++ src/info_frame.py
@@ -1,7 +1,5 @@
-from tkinter import X, CENTER, N, W, S, E
 from datetime import datetime
-
-from customtkinter import CTk, CTkScrollableFrame, CTkFrame, CTkImage, CTkLabel, CTkButton, CTkFont
+from customtkinter import CTk, CTkScrollableFrame, CTkFrame, CTkImage, CTkLabel, CTkButton, CTkFont, CTkProgressBar
 from pytubefix import YouTube, Channel
 from src.helpers import get_links, center_window
 from requests import get
@@ -9,48 +7,97 @@ from PIL import Image
 from io import BytesIO
 
 
+class Table(CTkScrollableFrame):
+    """ Custom scrollable frame that holds DataFrames"""
+
+    def __init__(self, master, table_data, width=300, height=300, entry_padx=5, entry_pady=5, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+
+        self.table_data = table_data
+        self.configure(width=width, height=height)
+
+        for data_entry in self.table_data:
+            data_frame = DataFrame(self, data_entry)
+            data_frame.pack(padx=entry_padx, pady=entry_pady, fill='x')
+
+
 class DataFrame(CTkFrame):
     """ Custom frame widget that holds and displays data about the YouTube video. """
+
     def __init__(self, master, data, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
 
         self.data = data
-        # self.configure(height=50)
+        self.configure(height=50)
 
-        """LEFT SIDE"""
+        self.thumbnail = CTkLabel(self,
+                                  text='',
+                                  image=CTkImage(self.display_image(data[0]), size=(64, 36)))
 
-        # thumbnail (60x36)
-        self.thumbnail = CTkLabel(self, text='', image=CTkImage(self.display_image(data[0]), size=(64, 36)))
+        self.title = CTkLabel(self,
+                              text=self.data[1],
+                              height=12)
 
-        # title
-        self.title = CTkLabel(self, text=self.data[1], height=12)
+        self.channel = CTkLabel(self,
+                                text=self.data[2],
+                                font=CTkFont(size=11),
+                                height=8)
 
-        # channel name
-        self.channel = CTkLabel(self, text=self.data[2], font=CTkFont(size=11), height=8)
+        self.duration = CTkLabel(self,
+                                 text=f' {self.convert_time(self.data[3])}',
+                                 image=CTkImage(Image.open('../images/test/clock.png'), size=(12, 12)),
+                                 compound='left',
+                                 font=CTkFont(size=11),
+                                 height=8)
 
-        # duration
-        self.duration = CTkLabel(self, text=f' {self.convert_time(self.data[3])}', image=CTkImage(Image.open('../images/hourglass_12x12_white.png'), size=(12, 12)), compound='left', font=CTkFont(size=11), height=8)
+        self.views = CTkLabel(self,
+                              text=f' {self.data[4]}',
+                              image=CTkImage(Image.open('../images/test/webcam.png'), size=(12, 12)),
+                              compound='left',
+                              font=CTkFont(size=11),
+                              height=8)
 
-        # views
-        self.views = CTkLabel(self, text=f' {self.data[4]}', image=CTkImage(Image.open('../images/eye_12x12_white.png'), size=(12, 12)), compound='left', font=CTkFont(size=11), height=8)
+        self.uploaded_date = CTkLabel(self,
+                                      text=f' {self.convert_date(self.data[5])}',
+                                      image=CTkImage(Image.open('../images/test/calendar.png'), size=(12, 12)),
+                                      compound='left',
+                                      font=CTkFont(size=11),
+                                      height=8)
 
-        # uploaded_date
-        self.uploaded_date = CTkLabel(self, text=f' {self.convert_date(self.data[5])}', image=CTkImage(Image.open('../images/calendar_12x12_white.png'), size=(12, 12)), compound='left', font=CTkFont(size=11), height=8)
+        self.extension = CTkLabel(self,
+                                  text='test',
+                                  width=20,
+                                  fg_color='grey25',
+                                  corner_radius=5)
 
-        # RIGHT SIDE
-        self.extension = None
-        self.delete_btn = None
+        self.delete_btn = CTkButton(self,
+                                    text='',
+                                    width=24,
+                                    image=CTkImage(Image.open('../images/test/bin.png'), size=(24, 24)),
+                                    fg_color='transparent',
+                                    hover_color='grey20')
+
+        self.progress_bar = CTkProgressBar(self, width=954, height=2)
 
         # draw elements
         self.draw_elements()
 
     def draw_elements(self):
-        self.thumbnail.grid(column=0, row=0, rowspan=2, padx=10, pady=10, sticky=W)
-        self.title.grid(column=1, row=0, columnspan=800, pady=(0, 1), sticky=(S, W))
-        self.channel.grid(column=2, row=1, padx=(0, 5),  pady=(6, 4), sticky=W)
-        self.duration.grid(column=3, row=1, padx=5,  pady=(6, 4), sticky=W)
-        self.views.grid(column=4, row=1, padx=5,  pady=(6, 4), sticky=W)
-        self.uploaded_date.grid(column=5, row=1, padx=5, pady=(6, 4), sticky=W)
+        self.thumbnail.place(x=7, y=7)
+        self.update()
+        self.title.place(x=self.thumbnail.winfo_x() + self.thumbnail.winfo_width() + 10, y=7)
+        self.channel.place(x=self.thumbnail.winfo_x() + self.thumbnail.winfo_width() + 10, y=29)
+        self.update()
+        self.duration.place(x=self.channel.winfo_x() + self.channel.winfo_width() + 10, y=29)
+        self.update()
+        self.views.place(x=self.duration.winfo_x() + self.duration.winfo_width() + 10, y=29)
+        self.update()
+        self.uploaded_date.place(x=self.views.winfo_x() + self.views.winfo_width() + 10, y=29)
+
+        self.extension.place(relx=.9, rely=.5, anchor='center')
+        self.delete_btn.place(relx=.96, rely=.5, anchor='center')
+
+        self.progress_bar.place(x=7, y=48)
 
     @staticmethod
     def display_image(url):
@@ -83,22 +130,19 @@ if __name__ == '__main__':
     for link in url_list:
         yt = YouTube(link)
         table_data_list.append([
-                yt.thumbnail_url,
-                yt.title,
-                Channel(yt.channel_url).channel_name,
-                yt.length,
-                yt.views,
-                yt.publish_date
-            ])
+            yt.thumbnail_url,
+            yt.title,
+            Channel(yt.channel_url).channel_name,
+            yt.length,
+            yt.views,
+            yt.publish_date
+        ])
 
     root = CTk()
-    center_window(root, 800, 500)
+    center_window(root, 1000, 600)
+    root.resizable(False, False)
 
-    scr_frame = CTkScrollableFrame(root, width=800, height=300, fg_color='black')
-    scr_frame.pack()
-
-    for data_entry in table_data_list:
-        data_frame = DataFrame(scr_frame, data_entry)
-        data_frame.pack(padx=5, pady=5, fill=X)
+    table = Table(root, table_data_list, height=400, fg_color='black')
+    table.pack(fill='x')
 
     root.mainloop()
