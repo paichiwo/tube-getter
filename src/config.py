134375132e2b96be6e942b96b72430917058aaeb
paichiwo <lzerucha@gmail.com>
2024-05-02 18:42:56 +0100

    Add new images paths

--- src/config.py
+++ src/config.py
@@ -1,20 +1,20 @@
 from src.helpers import resource_path
 
 
-VERSION = '1.2.9'
+VERSION = '2.0 rc1'
 
 IMG_PATHS = {
-    'settings': resource_path('images/setting.png'),
     'icon': resource_path('images/yt3d.ico'),
     'icon_png': resource_path('images/yt3d.png'),
+    'settings': resource_path('images/setting.png'),
     'github': resource_path('images/github.png'),
     'switch_left': resource_path('images/switch_left.png'),
     'switch_right': resource_path('images/switch_right.png'),
-
-    'clock': resource_path('images/test/clock.png'),
-    'eye': resource_path('images/test/webcam.png'),
-    'calendar': resource_path('images/test/calendar.png'),
-    'bin': resource_path('images/test/bin.png'),
+    'clock': resource_path('images/clock.png'),
+    'eye': resource_path('images/webcam.png'),
+    'calendar': resource_path('images/calendar.png'),
+    'bin': resource_path('images/bin.png'),
+    'folder': resource_path('images/folder.png')
 }
 
 
@@ -29,4 +29,4 @@ I'm constantly working on updates, but should you find any bugs
 please visit my github by clicking an icon below and open an 'issue' to let me know.
 """
 
-GITHUB_URL = "https://github.com/paichiwo/tube-getter"
+GITHUB_URL = 'https://github.com/paichiwo/tube-getter'
