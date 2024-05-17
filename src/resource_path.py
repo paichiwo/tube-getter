c23fe1116bd9d46f9dad0543be8cb4ae1a46d691
paichiwo <lzerucha@gmail.com>
2024-05-17 16:16:40 +0100

    Create resource_path to avoid circular imports

---
+++ src/resource_path.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+def resource_path(relative_path):
+    """Get the absolute path for auto-py-to-exe"""
+    if hasattr(sys, '_MEIPASS'):
+        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base_path, relative_path)
+    return os.path.join(os.path.abspath('.'), relative_path)
