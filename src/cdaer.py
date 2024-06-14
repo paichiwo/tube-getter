73f1d7d07e9d85ff2944ecc3adc604ea30274440
paichiwo <lzerucha@gmail.com>
2024-06-14 16:32:19 +0100

    Create cda_convert method

--- src/cdaer.py
+++ src/cdaer.py
@@ -1,6 +1,6 @@
 import os
 import urllib.error
-from pycda import PyCDA
+from cda_download import CdaDownload
 from src.media_root import MediaRoot
 from src.config import INFO_MSG
 from src.helpers import (get_links, format_file_size, load_settings, format_filename, convert_to_mp3,
@@ -25,7 +25,7 @@ class CDAer(MediaRoot):
         self.enable_buttons()
 
     def get_cda_data_for_table(self):
-        cda = PyCDA(self.url_list[0])
+        cda = CdaDownload(self.url_list[0])
         self.table_list.append([
             cda.thumbnail(),
             cda.title(),
@@ -42,7 +42,7 @@ class CDAer(MediaRoot):
         output_path = load_settings()
 
         for i, link in enumerate(self.url_list):
-            cda = PyCDA(url=link)
+            cda = CdaDownload(url=link)
             filename = str(os.path.join(output_path, format_filename(cda.title())))
             file_exists = os.path.exists(f'{filename}.mp4') or (
                     self.dl_format == 'audio' and os.path.exists(f'{filename}.mp3'))
@@ -63,10 +63,20 @@ class CDAer(MediaRoot):
                 self.info_msg(INFO_MSG['dl_complete'])
 
     def cda_convert(self, filename, i):
-        pass
+        self.info_msg(INFO_MSG['converting'])
+        convert_to_mp3(f'{filename}.mp4', f'{filename}.mp3')
+        os.remove(f'{filename}.mp4')
+        self.table.frames[i].extension.configure(text='mp3')
+        self.table.frames[i].size.configure(text=f'{format_file_size(os.path.getsize(f'{filename}.mp3'))}')
+        self.table.frames[i].change_delete_btn()
+
+        if os.path.exists(filename + '.mp3'):
+            self.table.frames[i].change_delete_btn()
+            self.dl_speed.configure(text=self.initial_speed)
+            self.info_msg(INFO_MSG['conversion_done'])
 
     def cda_progress_callback(self, count, block_size, total_size):
-        pass
+
 
     def update_progress_bars(self):
         pass
