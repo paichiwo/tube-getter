ff8c5d98f79e546b7d50eb1e9af6b6c96be9670a
paichiwo <lzerucha@gmail.com>
2024-06-15 13:01:48 +0100

    BUGFIX: Fix filename creation for video and audio streams

--- src/youtuber.py
+++ src/youtuber.py
@@ -6,29 +6,18 @@ from pytubefix import YouTube, Channel
 from src.config import INFO_MSG
 from src.helpers import (get_links, convert_time, format_date, format_file_size, load_settings,
                          handle_audio_extension, format_dl_speed_string)
+from src.media_root import MediaRoot
 
 
-class YouTuber:
-    def __init__(self, dl_format, url_list, table_list, add_update_with_new_data, enable_buttons, info_msg,
-                 dl_speed, table):
-
-        self.url_list = url_list
-        self.table_list = table_list
-        self.info_msg = info_msg
-        self.add_update_with_new_data = add_update_with_new_data
-        self.enable_buttons = enable_buttons
-        self.dl_format = dl_format
-        self.dl_speed = dl_speed
-        self.table = table
-
-        self.initial_speed = '0 KiB/s'
-        self.download_start_time = datetime.now()
+class YouTuber(MediaRoot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def add(self, url):
         try:
             get_links(url, self.url_list)
             self.info_msg(INFO_MSG['gathering_data'])
-            data = self.get_yt_data_for_table()
+            data = self.create_yt_media_table()
             self.add_update_with_new_data(data)
         except pytubefix.exceptions.VideoUnavailable:
             self.info_msg(INFO_MSG['age_restricted_err'])
@@ -39,7 +28,7 @@ class YouTuber:
         finally:
             self.enable_buttons()
 
-    def get_yt_data_for_table(self):
+    def create_yt_media_table(self):
         self.table_list.clear()
         for url in self.url_list:
             yt = YouTube(url)
@@ -71,16 +60,17 @@ class YouTuber:
         return self.table_list
 
     def download(self):
-        filename = ''
         output_path = load_settings()
         for i, link in enumerate(self.url_list):
-            yt = YouTube(link, on_progress_callback=self.yt_progress_callback)
+            yt = YouTube(link, on_progress_callback=self.progress_callback)
             if self.dl_format == 'audio':
                 yt_stream = yt.streams.get_audio_only()
                 filename = handle_audio_extension(yt_stream)
             else:
                 yt_stream = yt.streams.get_highest_resolution()
+                filename = yt_stream.title + '.mp4'
             self.info_msg(INFO_MSG['downloading'])
+
             try:
                 if os.path.exists(os.path.join(load_settings(), filename)):
                     self.table.frames[i].change_delete_btn()
@@ -92,14 +82,18 @@ class YouTuber:
             except (PermissionError, RuntimeError):
                 self.info_msg(INFO_MSG['permission_err'])
 
-    def yt_progress_callback(self, stream, chunk, bytes_remaining):
+    def progress_callback(self, stream, chunk, bytes_remaining):
         total_size = stream.filesize
         bytes_downloaded = total_size - bytes_remaining
         percentage = bytes_downloaded / total_size
         elapsed_time = (datetime.now() - self.download_start_time).total_seconds()
         download_speed = bytes_downloaded / (1024 * elapsed_time)
+
+        self.update_progress_bars(percentage, download_speed)
+
+    def update_progress_bars(self, percentage, download_speed):
         for i, item in enumerate(self.table_list):
-            if item[1] == stream.title:
-                self.table.frames[i].progress_bar.set(percentage)
-                self.table.frames[i].change_delete_btn()
-                self.dl_speed.configure(text=format_dl_speed_string(download_speed))
+            # if item[1] == stream.title:
+            self.table.frames[i].progress_bar.set(percentage)
+            self.table.frames[i].change_delete_btn()
+            self.dl_speed.configure(text=format_dl_speed_string(download_speed))
