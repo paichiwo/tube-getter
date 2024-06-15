fdc93ec063c7e7ff36617f17f1976a4dfe69c18e
paichiwo <lzerucha@gmail.com>
2024-06-15 12:58:38 +0100

    Create use_ffmpeg that uses ffmpeg to convert directly. Create progress callback for ffmpeg

--- src/helpers.py
+++ src/helpers.py
@@ -1,13 +1,14 @@
 import json
 import os
 import re
+import subprocess
 import requests
 import zipfile
+import threading
 from datetime import datetime
 from pytubefix import Playlist
 from customtkinter import CTkImage
 from PIL import Image
-from pydub import AudioSegment
 from bs4 import BeautifulSoup
 from src.config import GITHUB_URL
 from src.resource_path import resource_path
@@ -33,6 +34,7 @@ def center_window(window, width, height):
 
 
 def imager(path, x, y):
+    """Easy way to create image with CTK"""
     return CTkImage(Image.open(path), size=(x, y))
 
 
@@ -47,7 +49,7 @@ def get_links(url, array):
 
 
 def load_settings():
-    # Load settings from JSON file
+    """Load settings from JSON file"""
     path = os.path.join(os.environ['LOCALAPPDATA'], 'Tube-Getter', 'settings.json')
     try:
         with open(path, 'r') as file:
@@ -57,7 +59,7 @@ def load_settings():
 
 
 def save_settings(data, file_name):
-    # Save settings to JSON file
+    """Save settings to JSON file"""
     path = os.path.join(os.environ['LOCALAPPDATA'], 'Tube-Getter')
     settings_path = os.path.join(path, file_name)
     if not os.path.exists(path):
@@ -67,6 +69,7 @@ def save_settings(data, file_name):
 
 
 def open_downloads_folder():
+    """Use os to open target folder"""
     os.startfile(load_settings())
 
 
@@ -79,6 +82,7 @@ def format_file_size(size_bytes) -> str:
 
 
 def format_dl_speed_string(download_speed):
+    """Format dl speed to KB/s and MB/s"""
     if download_speed < 1000:
         return f'{download_speed:.2f} KiB/s'
     else:
@@ -86,6 +90,7 @@ def format_dl_speed_string(download_speed):
 
 
 def format_filename(filename):
+    """Remove special characters and capitalize filename string"""
     chars_removed_title = re.sub(r'[^\w ]', '', filename)
     words = chars_removed_title.split()
     new_words = []
@@ -95,10 +100,12 @@ def format_filename(filename):
 
 
 def format_date(date):
+    """Format date to DD-MM-YYYY"""
     return datetime.strptime(str(date).split(' ')[0], '%Y-%m-%d').strftime('%d-%m-%Y')
 
 
 def convert_time(time_in_sec):
+    """Convert time in seconds to HH:MM:SS"""
     hours = time_in_sec // 3600
     time_in_sec %= 3600
     minutes = time_in_sec // 60
@@ -107,6 +114,7 @@ def convert_time(time_in_sec):
 
 
 def handle_audio_extension(stream):
+    """Create string with mp3 in case YouTube audio downloads as mp4"""
     if stream.mime_type == 'audio/mp4':
         return stream.default_filename.rsplit('.', 1)[0] + '.mp3'
     else:
@@ -114,17 +122,63 @@ def handle_audio_extension(stream):
 
 
 def unzip_ffmpeg():
-    with zipfile.ZipFile(resource_path('./ffmpeg/ffmpeg.zip'), 'r') as zip_ref:
-        zip_ref.extract('ffmpeg.exe', './ffmpeg/')
+    """Unzip ffmpeg file"""
+    dir_path = os.path.join(os.environ['LOCALAPPDATA'], 'Tube-Getter')
+    exe_path = os.path.join(dir_path, 'ffmpeg.exe')
+
+    if not os.path.exists(exe_path):
+        os.makedirs(dir_path, exist_ok=True)
+
+    with zipfile.ZipFile(resource_path('ffmpeg/ffmpeg.zip'), 'r') as zip_ref:
+        zip_ref.extract('ffmpeg.exe', dir_path)
 
 
-def convert_to_mp3(mp4_filepath, mp3_filepath):
-    AudioSegment.converter = resource_path('./ffmpeg/ffmpeg.exe')
-    audio = AudioSegment.from_file(mp4_filepath, format='mp4')
-    audio.export(mp3_filepath, format='mp3', bitrate=128)
+def convert_to_mp3(input_file, output_file, progress_callback=None):
+    """Use ffmpeg to convert mp4 to mp3. Output progress"""
+    ffmpeg_path = os.path.join(os.environ['LOCALAPPDATA'], 'Tube-Getter', 'ffmpeg.exe')
+
+    ffmpeg_command = [ffmpeg_path, '-i', input_file, '-b:a', '128k', '-y', output_file]
+
+    process = subprocess.Popen(
+        ffmpeg_command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        creationflags=subprocess.CREATE_NO_WINDOW,
+        universal_newlines=True)
+
+    duration = extract_duration(process.stderr)
+    track_progress(process.stderr, duration, progress_callback)
+
+    progress_thread = threading.Thread(target=track_progress,
+                                       args=(process.stderr, duration, progress_callback))
+    progress_thread.start()
+
+
+def extract_duration(ffmpeg_output):
+    """Extract duration information from ffmpeg output"""
+    for line in ffmpeg_output:
+        if 'Duration: ' in line:
+            match = re.search(r'Duration:\s+(\d{2}):(\d{2}):(\d{2}).\d+', line)
+            if match:
+                hours, minutes, seconds = map(int, match.groups())
+                return hours * 3600 + minutes * 60 + seconds
+
+
+def track_progress(ffmpeg_output, duration, progress_callback):
+    """Extract time progress information from ffmpeg output and call the progress_callback"""
+    for line in ffmpeg_output:
+        if 'time=' in line and duration:
+            match = re.search(r'time=\s*(\d{2}):(\d{2}):(\d{2}).\d+', line)
+            if match:
+                hours, minutes, seconds = map(int, match.groups())
+                current_time = hours * 3600 + minutes * 60 + seconds
+                progress = round(current_time / duration, 3)
+                progress_callback(progress)
 
 
 def check_for_new_version():
+    """Check for new version in GitHub repo"""
     try:
         response = requests.get(GITHUB_URL)
         response.raise_for_status()
